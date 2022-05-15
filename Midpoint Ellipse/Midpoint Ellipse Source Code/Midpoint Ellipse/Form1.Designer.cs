9548ae398b095eee6603da8524fddfd32271e017
Abdelrahman Ragab <56741841+Abdelrhman2022@users.noreply.github.com>
2022-05-16 01:08:22 +0200

    Midpoint Ellipse

---
+++ Midpoint Ellipse/Midpoint Ellipse Source Code/Midpoint Ellipse/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Midpoint_Ellipse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(389, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(399, 275);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Location = new System.Drawing.Point(635, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "DRAW";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(412, 306);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "RX";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "X0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "RY";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Y0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(16, 154);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(16, 212);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Midpoint Ellipse/Midpoint Ellipse Source Code/Midpoint Ellipse/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Midpoint_Ellipse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            this.Text = "Midpoint Ellipse";
+        }
+
+        public void Ellipse(int rx, int ry, int xc, int yc)
+        {
+            double rxx = Math.Sqrt(rx);
+            double ryy = Math.Sqrt(ry);
+            double xend = Math.Sqrt(xc);
+            double yend = Math.Sqrt(yc);
+
+            var sbrush = Brushes.Black;
+            var g = panel1.CreateGraphics();
+
+            double x, y;
+            double p;
+
+            x = 0;
+            y = ryy;
+            p = (ryy * ryy) - (rxx * rxx * ryy) + ((rxx * rxx) / 4);
+
+            while ((2 * x * ryy * ryy) < (2 * y * rxx * rxx))
+            {
+                g.FillRectangle(sbrush, (float)xend + (float)x, (float)yend - (float)y, 3, 3);
+                g.FillRectangle(sbrush, (float)xend - (float)x, (float)yend + (float)y, 3, 3);
+                g.FillRectangle(sbrush, (float)xend + (float)x, (float)yend + (float)y, 3, 3);
+                g.FillRectangle(sbrush, (float)xend - (float)x, (float)yend - (float)y, 3, 3);
+
+                if (p < 0)
+                {
+                    x = x + 1;
+                    p = p + (2 * ryy * ryy * x) + (ryy * ryy);
+                }
+                else
+                {
+                    x = x + 1;
+                    y = y - 1;
+                    p = p + (2 * ryy * ryy * x + ryy * ryy) - (2 * rxx * rxx * y);
+                }
+            }
+            p = ((float)x + 0.5) * ((float)x + 0.5) * ryy * ryy + (y - 1) * (y - 1) * rxx * rxx - rxx * rxx * ryy * ryy;
+
+            while (y >= 0)
+            {
+                g.FillRectangle(sbrush, (float)xend + (float)x, (float)yend - (float)y, 3, 3);
+                g.FillRectangle(sbrush, (float)xend - (float)x, (float)yend + (float)y, 3, 3);
+                g.FillRectangle(sbrush, (float)xend + (float)x, (float)yend + (float)y, 3, 3);
+                g.FillRectangle(sbrush, (float)xend - (float)x, (float)yend - (float)y, 3, 3);
+
+                if (p > 0)
+                {
+                    y = y - 1;
+                    p = p - (2 * rxx * rxx * y) + (rxx * rxx);
+
+                }
+                else
+                {
+                    y = y - 1;
+                    x = x + 1;
+                    p = p + (2 * ryy * ryy * x) - (2 * rxx * rxx * y) - (rxx * rxx);
+                }
+            }
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                int x = int.Parse(textBox1.Text); 
+                int x2 = int.Parse(textBox2.Text);
+                int y = int.Parse(textBox3.Text);
+                int y2 = int.Parse(textBox4.Text);
+
+                Ellipse(x, y, x2, y2);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter Intger Number", "Input error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.panel1.Invalidate();
+        }
+    }
+}
