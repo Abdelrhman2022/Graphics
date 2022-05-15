a533acfaf15e02e85701c64f4b18abd498b86786
Abdelrahman Ragab <56741841+Abdelrhman2022@users.noreply.github.com>
2022-05-16 01:31:33 +0200

    Line Bresenham

---
+++ Bresenham/Bresenham Source Code/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.x0textBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.y0textBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.x1textBox = new System.Windows.Forms.TextBox();
+            this.y1textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(87, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(592, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bresenham Line Drawing Algorithm";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.Location = new System.Drawing.Point(440, 90);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(322, 254);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X0";
+            // 
+            // x0textBox
+            // 
+            this.x0textBox.Location = new System.Drawing.Point(95, 141);
+            this.x0textBox.Name = "x0textBox";
+            this.x0textBox.Size = new System.Drawing.Size(57, 20);
+            this.x0textBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(232, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Y0";
+            // 
+            // y0textBox
+            // 
+            this.y0textBox.Location = new System.Drawing.Point(329, 143);
+            this.y0textBox.Name = "y0textBox";
+            this.y0textBox.Size = new System.Drawing.Size(57, 20);
+            this.y0textBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(42, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "X1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(232, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Y1";
+            // 
+            // x1textBox
+            // 
+            this.x1textBox.Location = new System.Drawing.Point(95, 242);
+            this.x1textBox.Name = "x1textBox";
+            this.x1textBox.Size = new System.Drawing.Size(57, 20);
+            this.x1textBox.TabIndex = 8;
+            // 
+            // y1textBox
+            // 
+            this.y1textBox.Location = new System.Drawing.Point(329, 244);
+            this.y1textBox.Name = "y1textBox";
+            this.y1textBox.Size = new System.Drawing.Size(57, 20);
+            this.y1textBox.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Maroon;
+            this.button1.Location = new System.Drawing.Point(236, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 38);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.Maroon;
+            this.button2.Location = new System.Drawing.Point(34, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 38);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.y1textBox);
+            this.Controls.Add(this.x1textBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.y0textBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.x0textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bresenham";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox x0textBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox y0textBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox x1textBox;
+        private System.Windows.Forms.TextBox y1textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
